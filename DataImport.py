a23de44a8c602f92eb66138027f9829d68a6c63b
Hu <2072850636@qq.com>
2024-03-07 10:52:03 +0800

    数据导入完成

---
+++ DataImport.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def load_data():
+    """
+    :return: data的pandas对象
+    """
+    file_path = "Data/decrypt_data2.csv"
+    df = pd.read_csv(file_path,sep=",",encoding="GBK")
+    print(df)
+    return df
+
